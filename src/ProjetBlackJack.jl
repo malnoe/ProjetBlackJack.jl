71782fb0c750ff0d3240820e19cc99254e5fe20e
Garance Malnoë <106530489+malnoe@users.noreply.github.com>
2024-12-09 18:18:56 +0100

    instantiate des packages

--- src/ProjetBlackJack.jl
+++ src/ProjetBlackJack.jl
@@ -4,7 +4,7 @@ using Vizagrams, Pluto, PlutoUI, HypertextLiteral
 
 export game, Game, initialize_game, new!, turn!, end_game!, interaction
 
-includ("blackjack_julia/main_terminal.jl")
+include("blackjack_julia/main_terminal.jl")
 include("blackjack_julia/jeu_pluto_notebook.jl")
 
 end

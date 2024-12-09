e95c176ac65a515f540c334ca56da3ce50ca5ca8
Garance Malnoë <106530489+malnoe@users.noreply.github.com>
2024-12-09 18:03:09 +0100

    please work

--- src/ProjetBlackJack.jl
+++ src/ProjetBlackJack.jl
@@ -2,8 +2,9 @@ module ProjetBlackJack
 
 using Vizagrams, Pluto, PlutoUI, HypertextLiteral
 
-export Game, initialize_game, new!, turn!, end_game!, interaction
+export game, Game, initialize_game, new!, turn!, end_game!, interaction
 
-include("jeu_pluto_notebook.jl")
+includ("blackjack_julia/main_terminal.jl")
+include("blackjack_julia/jeu_pluto_notebook.jl")
 
 end

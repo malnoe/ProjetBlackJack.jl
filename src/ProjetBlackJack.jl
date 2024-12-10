54bb6c2c65b9b94955cede003e9fed971d37077f
da-n-ta <73463092+da-n-ta@users.noreply.github.com>
2024-12-10 15:17:58 +0100

    trial 2 r

--- src/ProjetBlackJack.jl
+++ src/ProjetBlackJack.jl
@@ -30,15 +30,35 @@ function init_r_env()
     end
 end
 
+function install_r_dependencies()
+    try
+        # Vérifie et installe les packages R nécessaires
+        R"""
+        required_packages <- c("dplyr")
+        installed_packages <- rownames(installed.packages())
+        for (pkg in required_packages) {
+            if (!pkg %in% installed_packages) {
+                install.packages(pkg, repos = "http://cran.us.r-project.org")
+            }
+        }
+        """
+        println("Les dépendances R ont été vérifiées et installées si nécessaire.")
+    catch e
+        error("Erreur lors de l'installation des dépendances R : $e")
+    end
+end
+
 # Fonction d'initialisation globale
 function init()
     init_python_env()
+    install_r_dependencies()
     init_r_env()
 end
 
 function run_r_cartes_et_deck()
     script_path = joinpath(@__DIR__, "..", "deps", "blackjack_r", "Cartes_et_Deck.R")
     R"""
+    library(dplyr)
     source("$script_path")
     """
 end
@@ -46,6 +66,7 @@ end
 function run_r_jeu()
     script_path = joinpath(@__DIR__, "..", "deps", "blackjack_r", "le_jeu.R")
     R"""
+    library(dplyr)
     source("$script_path")
     """
 end

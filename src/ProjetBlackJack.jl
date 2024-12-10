df7adb7163e792dc33a8159509fc880741b99744
da-n-ta <73463092+da-n-ta@users.noreply.github.com>
2024-12-10 15:09:01 +0100

    R trial

--- src/ProjetBlackJack.jl
+++ src/ProjetBlackJack.jl
@@ -2,9 +2,53 @@ module ProjetBlackJack
 
 using Vizagrams, Pluto, PlutoUI, HypertextLiteral
 
-export game, Game, initialize_game, new!, turn!, end_game!, interaction
+export game, Game, initialize_game, new!, turn!, end_game!, interaction, run_r_cartes_et_deck, run_r_jeu
 
 include("blackjack_julia/main_terminal.jl")
 include("blackjack_julia/jeu_pluto_notebook.jl")
 
+using PyCall
+using RCall
+
+# Initialisation de l'environnement Python
+function init_python_env()
+    try
+        
+        pyimport("sys")  # Vérifie que Python est disponible
+        println("Environnement Python initialisé avec succès.")
+    catch e
+        error("Échec de l'initialisation de l'environnement Python: $e")
+    end
+end
+
+# Initialisation de l'environnement R
+function init_r_env()
+    try
+        R"print('Initialisation de l'environnement R réussie')"  # Vérifie que R fonctionne
+    catch e
+        error("Échec de l'initialisation de l'environnement R: $e")
+    end
+end
+
+# Fonction d'initialisation globale
+function init()
+    init_python_env()
+    init_r_env()
+end
+
+function run_r_cartes_et_deck()
+    script_path = joinpath(@__DIR__, "..", "deps", "blackjack_r", "Cartes_et_Deck.R")
+    R"""
+    source("$script_path")
+    """
+end
+
+function run_r_jeu()
+    script_path = joinpath(@__DIR__, "..", "deps", "blackjack_r", "le_jeu.R")
+    R"""
+    source("$script_path")
+    """
+end
+
+init()
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,14 @@ using Test
 
 @testset "ProjetBlackJack.jl" begin
     # Write your tests here.
+
+
+end
+@testset "R Script Test" begin
+    try
+        run_r_jeu()
+        @test true  # Pass the test if no errors occur
+    catch e
+        @test false "R script failed: $e"
+    end
 end

65ff23c6dd779488dd6af7d86812ef5bb8eaf739
Garance Malnoë <106530489+malnoe@users.noreply.github.com>
2024-12-13 21:34:18 +0100

    correction help

--- src/ProjetBlackJack.jl
+++ src/ProjetBlackJack.jl
@@ -24,12 +24,15 @@ end
 
 function help()
     println("Fonction utiles :")
-    println("Blackjack Julia :")
-    print("ProjetBlackJack.PlayGame.game(), blackjack_notebook()")
-    println("Benchmarck :")
-    print(" ProjetBlackJack.Benchmark.cribble_erathostene_python(n),  ProjetBlackJack.Benchmark.cribble_erathostene_python_time(n),  ProjetBlackJack.Benchmark.cribble_erathostene_julia(n),  ProjetBlackJack.Benchmark.cribble_erathostene_julia_time(n)")
-    println("Dispatching :")
-    print(".......")
+    println(" ")
+    println("1. Blackjack Julia :")
+    println("ProjetBlackJack.PlayGame.game(), blackjack_notebook()")
+    println("")
+    println("2. Benchmarck :")
+    println(" ProjetBlackJack.Benchmark.cribble_erathostene_python(n),  ProjetBlackJack.Benchmark.cribble_erathostene_python_time(n),  ProjetBlackJack.Benchmark.cribble_erathostene_julia(n),  ProjetBlackJack.Benchmark.cribble_erathostene_julia_time(n)")
+    println("")
+    println("3. Single vs Multiple dispatching :")
+    println(".......")
 end
 
 end

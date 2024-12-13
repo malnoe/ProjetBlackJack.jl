a0e1de74787b99fc8015846db94cc1cd8df5853c
Garance Malnoë <106530489+malnoe@users.noreply.github.com>
2024-12-13 18:51:12 +0100

    try2

--- src/ProjetBlackJack.jl
+++ src/ProjetBlackJack.jl
@@ -8,14 +8,16 @@ include("blackjack_julia/main_terminal.jl")
 include("blackjack_julia/jeu_pluto_notebook.jl")
 
 function blackjack_notebook()
-    # Specify the path to the example notebook
-    notebook_path = joinpath(@__DIR__,"examples", "blackjack_notebook.jl")
-    if isfile(notebook_path)
-        Pluto.run(notebook=notebook_path)
-    else
-        error("Notebook file not found: $notebook_path")
-    end
+    # URL vers le fichier notebook sur ton dépôt GitHub
+    notebook_url = "https://raw.githubusercontent.com/malnoe/ProjetBlackJack.jl/master/examples/blackjack_notebook.jl"
+    
+    # Emplacement temporaire pour télécharger le fichier
+    temp_notebook_path = joinpath(tempdir(), "blackjack_notebook.jl")
+
+    # Télécharger le fichier
+    Downloads.download(notebook_url, temp_notebook_path)
+
+    # Ouvrir le notebook avec Pluto
+    Pluto.run(notebook=temp_notebook_path)
 end
-
-
 end

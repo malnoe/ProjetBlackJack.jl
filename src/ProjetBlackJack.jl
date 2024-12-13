a43ed036cc718fb38c167799e290e2dc8e4d492b
Garance Malnoë <106530489+malnoe@users.noreply.github.com>
2024-12-13 18:11:15 +0100

    ouverture du notebook depuis le terminal

--- src/ProjetBlackJack.jl
+++ src/ProjetBlackJack.jl
@@ -2,9 +2,20 @@ module ProjetBlackJack
 
 using Vizagrams, Pluto, PlutoUI, HypertextLiteral
 
-export game, Game, initialize_game, new!, turn!, end_game!, interaction
+export game, Game, initialize_game, new!, turn!, end_game!, interaction, blackjack_notebook
 
 include("blackjack_julia/main_terminal.jl")
 include("blackjack_julia/jeu_pluto_notebook.jl")
+
+function blackjack_notebook()
+    # Specify the path to the example notebook
+    notebook_path = joinpath(@__DIR__, "examples", "blackjack_notebook.jl")
+    if isfile(notebook_path)
+        Pluto.run(notebook_path)
+    else
+        error("Notebook file not found: $notebook_path")
+    end
+end
+
 
 end

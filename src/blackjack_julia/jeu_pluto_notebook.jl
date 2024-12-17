1c146df7de2b1eac2af7ae1fe1967c52680539af
lancelot-r <lancelotravier@gmail.com>
2024-12-17 15:40:00 +0100

    Update jeu_pluto_notebook.jl

--- src/blackjack_julia/jeu_pluto_notebook.jl
+++ src/blackjack_julia/jeu_pluto_notebook.jl
@@ -117,7 +117,7 @@ function Vizagrams.draw(game::Game)
         text_end_game = "Choose an action!"
     end
 
-    full_text = text_player_hand * " ($hand_value_player) " * "\n" * text_dealer_hand * " ($hand_value_dealer) " * "\n" * text_end_game
+    full_text = text_player_hand * " ($hand_value_player) " + "\n" * text_dealer_hand * " ($hand_value_dealer) " + "\n" * text_end_game
   
     d = TextMark(text=full_text)
     return draw(d, height=16, anchor=:c)
